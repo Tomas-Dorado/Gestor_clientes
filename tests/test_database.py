b9c5f42cb15476e895f66365119639f19d50c12b
Tomas-Dorado <tdoradog@student.universidadunie.com>
2025-05-02 16:42:53 +0000

    Se modifico el test

--- tests/test_database.py
+++ tests/test_database.py
@@ -11,25 +11,27 @@ class Testdatabase(unittest.TestCase):
         ]
 
     def test_buscar_cliente(self):
-        cliente_existente = db.Clientes.buscar('15J')
-        cliente_no_existente = db.Clientes.buscar('99X')
+        cliente_existente = db.Clientes.buscar("87654321")
+        cliente_no_existente = db.Clientes.buscar("87654343")
         self.assertIsNotNone(cliente_existente)
         self.assertIsNone(cliente_no_existente)
+
     def test_crear_cliente(self):
-        nuevo_cliente = db.Clientes.crear('39X', 'Héctor', 'Costa')
+        nuevo_cliente = db.Clientes.crear('Héctor', 'Costa', "34665453")
         self.assertEqual(len(db.Clientes.lista), 4)
-        self.assertEqual(nuevo_cliente.dni, '39X')
+        self.assertEqual(nuevo_cliente.dni, '34665453')
         self.assertEqual(nuevo_cliente.nombre, 'Héctor')
         self.assertEqual(nuevo_cliente.apellido, 'Costa')
+
     def test_modificar_cliente(self):
-        cliente_a_modificar = copy.copy(db.Clientes.buscar('28Z'))
-        cliente_modificado = db.Clientes.modificar('28Z', 'Mariana',
-        'Pérez')
+        cliente_a_modificar = copy.copy(db.Clientes.buscar('"87654321"'))
+        cliente_modificado = db.Clientes.modificar('87654321', 'Mariana', 'Gonzalez')
         self.assertEqual(cliente_a_modificar.nombre, 'Ana')
         self.assertEqual(cliente_modificado.nombre, 'Mariana')
+
     def test_borrar_cliente(self):
-        cliente_borrado = db.Clientes.borrar('48H')
-        cliente_rebuscado = db.Clientes.buscar('48H')
+        cliente_borrado = db.Clientes.borrar("12345678")
+        cliente_rebuscado = db.Clientes.buscar("12345678")
         self.assertNotEqual(cliente_borrado, cliente_rebuscado)
 
 
