ed07af5683a5dfcd0b0914448da077d33f799783
Tomas-Dorado <tdoradog@student.universidadunie.com>
2025-05-04 19:11:17 +0000

    Se mejoro el menu y se añadio funciones en helpers

--- helpers.py
+++ helpers.py
@@ -1,1 +1,15 @@
 #Este fichero es el que tendra funciones auxiliares
+import os
+import platform
+def limpiar_pantalla():
+    os.system('cls') if platform.system() == "Windows" else os.system('clear')
+
+def leer_texto(longitud_min=0, longitud_max=100, mensaje=None):
+    print(mensaje) if mensaje else None
+    while True:
+        texto = input("> ")
+        if len(texto) < longitud_min or len(texto) > longitud_max:
+            print(f"El texto debe tener entre {longitud_min} y {longitud_max} caracteres.")
+            continue
+        return texto
+
